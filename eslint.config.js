f4949adc874a99a9fbdc37711a97b1de9e97ede1
Dyrean <fcanbeyy@gmail.com>
2024-07-19 23:54:41 +0300

    5-3 done

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,11 @@ import antfu from "@antfu/eslint-config"
 
 export default antfu({
 	stylistic: {
-		indent: "tab", // 4, or 'tab'
-		quotes: "double", // or 'double'
+		indent: "tab",
+		quotes: "double",
 	},
 	astro: true,
 	typescript: true,
+	jsonc: true,
+	yaml: true,
 })
